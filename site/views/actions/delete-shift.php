39f3b8f710b658765b9cb7c3818020dde57eba1c
waimea-wlpapps <wlpapps@waimea.school.nz>
2024-08-16 22:47:13 +1200

    Finished code for the website

--- site/views/actions/delete-shift.php
+++ site/views/actions/delete-shift.php
@@ -11,26 +11,6 @@ $id = $_GET['id'];
 
 $db = connectToDB();
 
-// $query = 'DELETE FROM shifts WHERE id = ? '; 
-
-// try {
-//     $stmt = $db ->prepare($query);
-//     $stmt ->execute([$id]);
-// }
-
-// catch (PDOException $e) {
-//     consoleLog($e->getMessage(), 'DB insert', ERROR);
-//     die('There was an error');
-// }
-// echo '<h2>Shift Deleted!</h2>' ;
-// echo '<p><a href="shifts?id=' . $id . '>"Return to shifts!</a></p>';
-
-// // echo  '<a href="shifts?id=' . $user['id'] . '&forename='.$user['forename'] .'&surname='.$user['surname'] .'"
-// // '.'Delete Shifts:</a>';
-// // echo '</td>';
-
-// echo '<p><a href="shifts?id="' . $id . '">View Users!</a>' ;
-
 $query = 'DELETE FROM shifts WHERE id = ? '; 
 
 try {
